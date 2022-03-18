84a693a0565d29a797c6d8755feaa2d88765deac
Afvanie <ardyansah201@gmail.com>
2022-03-18 14:55:32 +0700

    merubah header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -9,17 +9,10 @@
                     <div class="navbar-nav ml-auto">
                         <a href="index.html" class="nav-item nav-link active">Home</a>
                         <a href="about.html" class="nav-item nav-link">About</a>
-                        <a href="feature.html" class="nav-item nav-link">Feature</a>
-                        <a href="team.html" class="nav-item nav-link">Chef</a>
                         <a href="menu.html" class="nav-item nav-link">Menu</a>
-                        <a href="booking.html" class="nav-item nav-link">Booking</a>
-                        <div class="nav-item dropdown">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                            <div class="dropdown-menu">
-                                <a href="blog.html" class="dropdown-item">Blog Grid</a>
-                                <a href="single.html" class="dropdown-item">Blog Detail</a>
-                            </div>
-                        </div>
+                        <a href="team.html" class="nav-item nav-link">Chef</a>
+                        <a href="feature.html" class="nav-item nav-link">Pelanggan</a>
+                        <a href="booking.html" class="nav-item nav-link">Supplier</a>
                         <a href="contact.html" class="nav-item nav-link">Contact</a>
                     </div>
                 </div>
