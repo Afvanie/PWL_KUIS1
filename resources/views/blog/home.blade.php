4a671a001df63f710b8e06a541ea11c2c9fd04fa
Afvanie <ardyansah201@gmail.com>
2022-03-18 14:48:28 +0700

    Merubah konten Home

--- resources/views/blog/home.blade.php
+++ resources/views/blog/home.blade.php
@@ -1,13 +1,42 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <h3> Dashboard </h3>
-                        Login Sukses !<br>
-                        Selamat Datang di Website ini!
-        </div>
-    </div>
-</div>
+<div class="carousel-img">
+                            <img src="style/img/carousel-1.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h1>Bahan <span>Berkualitas</span> Terbaik</h1>
+                            <p>
+                                Burger King hanya menggunakan bahan berkualitas terbaik sehingga rasa yang dihasilkan sangat lezat
+                            </p>
+                            <div class="carousel-btn">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="style/img/carousel-2.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h1>Chef <span>Terbaik</span> Didunia</h1>
+                            <p>
+                            Burger King mempekerjakan chef terbaik didunia demi menjaga kualitas makanan yang dibuat
+                            </p>
+                            <div class="carousel-btn">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="carousel-img">
+                            <img src="style/img/carousel-3.jpg" alt="Image">
+                        </div>
+                        <div class="carousel-text">
+                            <h1>Pesan Antar <span>Tercepat</span></h1>
+                            <p>
+                                Burger King juga menyediakan jasa pesan antar dengan cepat makanan akan sampai dirumah anda
+                            </p>
+                            <div class="carousel-btn">
+                            </div>
+                        </div>
+                    </div>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -38,50 +38,7 @@
             <div class="container-fluid">
                 <div class="owl-carousel">
                     <div class="carousel-item">
-                        <div class="carousel-img">
-                            <img src="style/img/carousel-1.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-text">
-                            <h1>Best <span>Quality</span> Ingredients</h1>
-                            <p>
-                                Lorem ipsum dolor sit amet elit. Phasellus ut mollis mauris. Vivamus egestas eleifend dui ac consequat at lectus in malesuada
-                            </p>
-                            <div class="carousel-btn">
-                                <a class="btn custom-btn" href="">View Menu</a>
-                                <a class="btn custom-btn" href="">Book Table</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="carousel-img">
-                            <img src="style/img/carousel-2.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-text">
-                            <h1>World’s <span>Best</span> Chef</h1>
-                            <p>
-                                @yield('content')
-                            </p>
-                            <div class="carousel-btn">
-                                <a class="btn custom-btn" href="">View Menu</a>
-                                <a class="btn custom-btn" href="">Book Table</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <div class="carousel-img">
-                            <img src="style/img/carousel-3.jpg" alt="Image">
-                        </div>
-                        <div class="carousel-text">
-                            <h1>Fastest Order <span>Delivery</span></h1>
-                            <p>
-                                Sed ultrices, est eget feugiat accumsan, dui nibh egestas tortor, ut rhoncus nibh ligula euismod quam. Proin pellentesque odio
-                            </p>
-                            <div class="carousel-btn">
-                                <a class="btn custom-btn" href="">View Menu</a>
-                                <a class="btn custom-btn" href="">Book Table</a>
-                            </div>
-                        </div>
-                    </div>
+                        @yield('content')
                 </div>
             </div>
         </div>
